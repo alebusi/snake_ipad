84cc9341366e61e7401c400e50893d94e34d9e55
alebusi <allebusi@gmail.com>
2018-08-08 12:33:46 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,6 +1,6 @@
 (function() {
   var SIZE = 600; // Size of the play-field in pixels
-  var GRID_SIZE = SIZE / 30;
+  var GRID_SIZE = SIZE / 20;
   var c = document.getElementById('c');
   c.height = c.width = SIZE * 2; // 2x our resolution so retina screens look good
   c.style.width = c.style.height = SIZE + 'px';
