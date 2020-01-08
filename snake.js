a7fcd403c8d0a480193fc505fe97ebdfde0cd209
alebusi <allebusi@gmail.com>
2020-01-08 10:53:28 +0100

    Update snake.js

--- snake.js
+++ snake.js
@@ -105,7 +105,6 @@ function testbarraSpaz(e) {
 }
 
 function lanciaGioco() {
-   clearInterval(myVar);
    direction = newDirection = 1; // -2: up, 2: down, -1: left, 1: right
    snakeLength = 1;
    snake = [{x: SIZE / 2, y: SIZE / 2}]; // Snake starts in the center
